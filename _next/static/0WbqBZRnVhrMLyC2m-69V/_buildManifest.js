4681bd07b5cae83683dce70967b67c58b13cc816
liuyuxuan <ts02315607@gmail.com>
2026-09-18 14:30:17 +0800

    fix: drop unsupported audio preload links

---
+++ _next/static/0WbqBZRnVhrMLyC2m-69V/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/wedding-lottery//_next/:path+",
+        "destination": "/wedding-lottery/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
